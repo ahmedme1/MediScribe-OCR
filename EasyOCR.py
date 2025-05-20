0bf9e18ac7e1b4f959e2f2983f752814f3ff9ff0
Shriram Mange <shrirammange12345@gmail.com>
2025-05-20 13:56:46 +0530

    removed extra files

--- EasyOCR.py
+++
@@ -1,9 +0,0 @@
-import easyocr
-
-def ocr_easyocr(image_path):
-    reader = easyocr.Reader(['en'])  # English Language
-    result = reader.readtext(image_path, detail=0)
-    return " ".join(result)
-
-# Ensure the file path is correct
-print(ocr_easyocr("Raw_Image_6.jpg"))
